6e6c92c1962ba2ffdacd24cddcb5eeb31270ae11
starfish <ac355deny@gmail.com>
2021-10-10 19:30:28 +0800

    NoMilestone v1.0.1

---
+++ NoMilestone/NoMilestone.cs
@@ -0,0 +1,68 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+
+namespace NoMilestone
+{
+    [BepInPlugin("com.starfi5h.plugin.NoMilestone", "NoMilestone", "1.0.0")]
+    public class NoMilestone : BaseUnityPlugin
+    {
+        Harmony harmony;
+        new public static ConfigFile Config;
+        public static ConfigEntry<bool> AchievementPopup;
+        public static ConfigEntry<bool> MilestonePopup;
+        public static ConfigEntry<bool> AchievementActive;
+        public static ConfigEntry<bool> MilestoneActive;        
+
+        public void Start()
+        {
+            harmony = new Harmony("com.starfi5h.plugin.NoMilestone");
+            harmony.PatchAll(typeof(Patches));
+            Config = base.Config;
+
+            AchievementPopup = Config.Bind<bool>("Pop-up", "Achievement", false, "Enable pop-up of achievement");
+            MilestonePopup = Config.Bind<bool>("Pop-up", "Milestone", false, "Enable pop-up of milestone");
+            AchievementActive = Config.Bind<bool>("Active", "Achievement", true, "Enable achievement system");
+            MilestoneActive = Config.Bind<bool>("Active", "Milestone", true, "Enable milestone system");
+        }
+
+        public void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+    }
+
+    public class Patches
+    {
+        [HarmonyPostfix, HarmonyPatch(typeof(GameMain), "Resume")]
+        internal static void GameMain_Resume()
+        {
+            NoMilestone.Config.Reload();
+        }
+
+
+        [HarmonyPrefix, HarmonyPatch(typeof(AchievementSystem), "NotifyAchievementChange")]
+        internal static bool AchievementSystem_NotifyAchievementChange()
+        {
+            return NoMilestone.AchievementPopup.Value;
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(MilestoneSystem), "NotifyUnlockMilestone")]
+        internal static bool MilestoneSystem_NotifyUnlockMilestone()
+        {
+            return NoMilestone.MilestonePopup.Value;
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(AchievementSystem), "active", MethodType.Getter)]
+        internal static void AchievementSystem_active(ref bool __result)
+        {
+            __result &= NoMilestone.AchievementActive.Value;
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(MilestoneLogic), "active", MethodType.Getter)]
+        internal static void MilestoneLogic_active(ref bool __result)
+        {
+            __result &= NoMilestone.MilestoneActive.Value;
+        }
+    }    
+}
---
+++ NoMilestone/README.md
@@ -0,0 +1,26 @@
+# No Milestone
+
+Prevents milestone and achievement pop-up windows from appearing.
+
+## Configuration
+
+Run the game one time to generate `.cfg` file. Pause and resume the game to make the changes take effect.
+
+- `Active`  
+Activate/Deactivate achievement system or milestone system.
+
+- `Pop-up`  
+Enable/Disable pop-up window of achievement or milestone.
+
+
+----
+
+## Changelog
+
+#### v1.0.0  
+\- Initial Release. (Game Version 0.8.22)
+
+----
+
+#### Acknowledgements
+All trademarks, copyright, and resources related to Dyson Sphere Project itself, remain the property of Gamera Game and Youthcat Studio as applicable according to the license agreement distributed with Dyson Sphere Program.  
