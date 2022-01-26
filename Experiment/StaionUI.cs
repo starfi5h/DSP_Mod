1f497022058a749f499d0d40b17bed4046b5562e
starfish <ac355deny@gmail.com>
2022-01-27 06:49:20 +0800

    report difference of display value and data

--- Experiment/StaionUI.cs
+++ Experiment/StaionUI.cs
@@ -11,7 +11,7 @@ namespace Experiment
         [HarmonyPatch(typeof(UIStationWindow), nameof(UIStationWindow.OnMaxChargePowerSliderValueChange))]
         internal static bool OnMaxChargePowerSliderValueChange_Prefix()
         {
-            //Log.Debug("cHANGE");
+            Log.Debug("change");
             if (Input.GetMouseButtonUp(0))
             {
                 Log.Info("UP");
@@ -23,15 +23,44 @@ namespace Experiment
             return false;
         }
 
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(UIStationWindow), nameof(UIStationWindow._OnUpdate))]
-        internal static void OnUpdate_Prefix(UIStationWindow __instance)
+
+
+
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(UIStationWindow), nameof(UIStationWindow.OnStationIdChange))]
+        internal static void OnStationIdChange_Postfix(UIStationWindow __instance)
         {
-            if (Input.GetMouseButtonUp(0))
-            {
-                Log.Info("UP");
-                __instance.OnStationIdChange();
-            }
+            Log.Info("OnStationIdChange");
+            //Log.Debug(Environment.StackTrace);
+        }
+
+        static bool canPass = false;
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UIStationStorage), nameof(UIStationStorage.OnMaxSliderValueChange))]
+        internal static bool OnMaxSliderValueChangePrefix(UIStationStorage __instance, float val)
+        {
+            //Log.Info($"OnMaxSliderValueChange {val}");
+            //Log.Debug(Environment.StackTrace);
+            return canPass;
+        }
+
+        static bool[] HasChanged;
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UIStationStorage), nameof(UIStationStorage._OnUpdate))]
+        public static void _OnUpdate_Prefix(UIStationStorage __instance, ref float __state)
+        {
+            __state = __instance.maxSlider.value;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(UIStationStorage), nameof(UIStationStorage._OnUpdate))]
+        public static void _OnUpdate_Postfix(UIStationStorage __instance, float __state)
+        {
+            HasChanged[__instance.index] = __instance.maxSlider.value != __state;
+            __instance.maxSlider.value = __state;
         }
 
         [HarmonyPostfix]
@@ -40,22 +69,39 @@ namespace Experiment
         {
             //Hide UI elements until sync data arrive
             Log.Info("OnOpen");
-            __instance.titleText.text = "Loading...";
+            //__instance.titleText.text = "Loading...";
             for (int i = 0; i < __instance.storageUIs.Length; i++)
             {
-                __instance.storageUIs[i]._Close();
-                __instance.storageUIs[i].ClosePopMenu();
+                //__instance.storageUIs[i]._Close();
+                //__instance.storageUIs[i].ClosePopMenu();
             }
-            __instance.panelDown.SetActive(false);
+            //__instance.panelDown.SetActive(false);
+            HasChanged = new bool[__instance.storageUIs.Length];
             Log.Info($"PlanetId {__instance.factory.planetId} {__instance.transport.stationPool[__instance.stationId].planetId}");
         }
 
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(UIStationWindow), nameof(UIStationWindow.OnStationIdChange))]
-        internal static void OnStationIdChange_Postfix(UIStationWindow __instance)
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UIStationWindow), nameof(UIStationWindow._OnUpdate))]
+        public static void OnUpdate_Prefix(UIStationWindow __instance)
         {
-            Log.Info("OnStationIdChange");
-            //Log.Debug(Environment.StackTrace);
+            if (Input.GetMouseButtonUp(0))
+            {
+                Log.Info("UP");
+                //Check storage max value changes
+                for (int index = 0; index < __instance.storageUIs.Length; index++)
+                {
+                    if (HasChanged[index])
+                    {
+                        Log.Debug(index);
+                        //Do the work in OnMaxSliderValueChange()
+                        StationStore stationStore = __instance.transport.stationPool[__instance.stationId].storage[index];
+                        float val = __instance.storageUIs[index].maxSlider.value;
+                        __instance.transport.SetStationStorage(__instance.stationId, index, stationStore.itemId, (int)(val * 100f + 0.5f), stationStore.localLogic, stationStore.remoteLogic, GameMain.mainPlayer);
+                        HasChanged[index] = false;
+                    }
+                }
+            }
         }
 
     }
