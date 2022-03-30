17d494220a95c3ee38643326aaf818f2c4bf7694
starfish <ac355deny@gmail.com>
2022-03-31 05:23:30 +0800

    first version

---
+++ ThreadOptimization/src/PerformanceMonitor_Patch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace ThreadOptimization
+{
+    class PerformanceStat_Patch
+    {
+        [HarmonyPostfix, HarmonyPatch(typeof(UIPerformancePanel), nameof(UIPerformancePanel._OnOpen))]
+        internal static void UIPerformancePanel_Prefix()
+        {
+            PerformanceMonitor.cpuWorkLevels[(int)ECpuWorkEntry.DysonSphere] = 2;
+            PerformanceMonitor.cpuWorkParents[(int)ECpuWorkEntry.DysonSphere] = ECpuWorkEntry.Factory;
+            PerformanceMonitor.cpuWorkLevels[(int)ECpuWorkEntry.DysonSwarm] = -1;
+            PerformanceMonitor.cpuWorkParents[(int)ECpuWorkEntry.DysonBullet] = ECpuWorkEntry.DysonSphere; //this should be a part of swarm
+            PerformanceMonitor.cpuWorkLevels[(int)ECpuWorkEntry.DysonRocket] = -1;
+        }
+    }
+}
---
+++ ThreadOptimization/src/ThreadSystem.cs
@@ -0,0 +1,91 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Unity;
+using UnityEngine;
+
+
+namespace ThreadOptimization
+{
+	public enum EMission
+	{
+		None,
+		Factory,
+		DysonRocket,
+		FactoryStat
+	}
+
+	static class ThreadSystem
+    {
+        readonly static List<Worker> workers = new List<Worker>();
+		static int count;
+
+        public static void Schedule(EMission mission, int workerCount)
+        {
+			count = workerCount;
+			for (int i = 0; i < count; i++)
+            {
+				if (workers.Count < count)
+					workers.Add(new Worker(i));
+				workers[i].Mission = mission;
+				workers[i].CompleteEvent.Reset();
+				ThreadPool.QueueUserWorkItem(workers[i].Callback);
+			}
+		}
+
+		public static void Complete()
+        {
+			for (int i = 0; i < count; i++)
+				workers[i].CompleteEvent.WaitOne();
+		}
+    }
+
+	class Worker
+	{
+		public EMission Mission { get; set; }
+		public int Index { get; }
+		public WaitCallback Callback { get; }
+		public AutoResetEvent CompleteEvent { get; }
+
+		public Worker(int index)
+		{
+			Index = index;
+			Callback = new WaitCallback(ComputerThread);
+			CompleteEvent = new AutoResetEvent(true);
+		}
+
+		public void ComputerThread(object state = null)
+		{
+			try
+			{
+				//var watch = new HighStopwatch();
+				//watch.Begin();
+				switch (Mission)
+				{
+					case EMission.Factory:
+						GameMain.data.factories[Index].GameTick(GameMain.gameTick);
+						break;
+
+					case EMission.DysonRocket:
+						GameMain.data.dysonSpheres[Index].RocketGameTick();
+						break;
+
+					case EMission.FactoryStat:
+						GameMain.data.statistics.production.factoryStatPool[Index].GameTick(GameMain.gameTick);
+						break;
+
+					default: break;
+				}
+				CompleteEvent.Set();
+				//Log.Debug($"[{Index,2}]: {watch.duration * 1000}");
+			}
+			catch (Exception e)
+			{
+				Log.Error($"Thread Error! mission:{Mission} index:{Index}");
+				Log.Error(e);
+			}
+		}
+	}
+
+}
