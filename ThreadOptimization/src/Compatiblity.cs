1a010acae50e3bfb178a8883aedf31702ba9d638
starfish <ac355deny@gmail.com>
2022-04-01 15:43:31 +0800

    compatibility

---
+++ ThreadOptimization/src/Compatiblity.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+// Make compatible to CommonAPI custom components functions
+// https://github.com/kremnev8/CommonAPI/blob/d39bf67004d26dd87c24346dd3c2d741a087d646/CommonAPI/Systems/PlanetExtensionSystem/Patches/PlanetExtensionHooks.cs
+
+namespace ThreadOptimization
+{
+    class Compatiblity
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(MultithreadSystem), "PreparePowerSystemFactoryData")]
+        [HarmonyPatch(typeof(MultithreadSystem), "PrepareAssemblerFactoryData")]
+        [HarmonyPatch(typeof(MultithreadSystem), "PrepareTransportData")]
+        internal static bool DummyFunctions()
+        {
+            return false;
+        }
+    }
+}
