9b61b555544fe4e4a2386dd98e7a4328199db94e
starfish <ac355deny@gmail.com>
2022-04-07 09:02:20 +0800

    ReorderTechQueue-v1.0.0

---
+++ ReorderTechQueue/README.md
@@ -0,0 +1,20 @@
+# ReorderTechQueue  
+
+
+![click-and-drag](https://raw.githubusercontent.com/starfi5h/DSP_Mod/master/ReorderTechQueue/img/demo1.gif)  
+Reorder research queue by pressing left click and dragging the icon to the target position.  
+
+## Installation
+Via [r2modman](https://dsp.thunderstore.io/package/ebkr/r2modman/), or manual download the file and put `ReorderTechQueue.dll` in `BepInEx/plugins` folder.
+
+----
+
+## Changelog
+
+#### v1.0.0  
+\- Initial release. (Game Version 0.9.24.11286)
+
+----
+
+#### Acknowledgements
+All trademarks, copyright, and resources related to Dyson Sphere Project itself, remain the property of Gamera Game and Youthcat Studio as applicable according to the license agreement distributed with Dyson Sphere Program.  
---
+++ ReorderTechQueue/ReorderTechQueue.cs
@@ -0,0 +1,103 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace ReorderTechQueue
+{
+    [BepInPlugin("com.starfi5h.plugin.ReorderTechQueue", "ReorderTechQueue", "1.0.0")]
+    public class ReorderTechQueuePlugin : BaseUnityPlugin
+    {
+        Harmony harmony;
+
+        public void Awake()
+        {
+            harmony = new Harmony("com.starfi5h.plugin.ReorderTechQueue");
+            harmony.PatchAll(typeof(ReorderTechQueue));
+        }
+
+        public void OnDestroy()
+        {
+            ReorderTechQueue.OnDestroy();
+            harmony.UnpatchSelf();
+        }
+    }
+
+    class ReorderTechQueue
+    {
+        [HarmonyPostfix, HarmonyPatch(typeof(UIResearchQueue), "_OnInit")]
+        public static void OnInit(UIResearchQueue __instance)
+        {
+            AddUIReorderNode(__instance.nodes);
+        }
+
+        public static void OnDestroy()
+        {
+            RemoveUIReorderNode(UIRoot.instance.uiGame.researchQueue.nodes);
+            RemoveUIReorderNode(UIRoot.instance.uiGame.techTree.resQueueUI.nodes);
+        }
+
+        private static void AddUIReorderNode(UIResearchQueueNode[] nodes)
+        {
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                GameObject gameObject = nodes[i].gameObject;
+                UIReorderNode component = gameObject.AddComponent<UIReorderNode>();
+                component.Index = i;
+            }
+        }
+
+        private static void RemoveUIReorderNode(UIResearchQueueNode[] nodes)
+        {
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                GameObject gameObject = nodes[i].gameObject;
+                GameObject.Destroy(gameObject.GetComponent<UIReorderNode>());
+            }
+        }
+
+        public class UIReorderNode : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler
+        {
+            static int currIndex = -1;
+            public int Index { get; set; }
+
+            public void OnPointerDown(PointerEventData pointerEventData)
+            {
+                currIndex = Index;
+            }
+
+            public void OnPointerUp(PointerEventData pointerEventData)
+            {
+                currIndex = -1;
+            }
+
+            public void OnPointerEnter(PointerEventData eventData)
+            {
+                if (currIndex != -1)
+                {
+                    ReorderQueue(currIndex, Index);
+                    currIndex = Index;
+                }
+            }
+
+            private static void ReorderQueue(int oldIndex, int newIndex)
+            {
+                int[] techQueue = GameMain.data.history.techQueue;
+                int techId = techQueue[oldIndex];
+                GameMain.data.history.RemoveTechInQueue(oldIndex);
+
+                int[] newQueue = new int[techQueue.Length - 1];
+                for (int i = newQueue.Length - 1; i >= newIndex; i--)
+                {
+                    newQueue[i] = techQueue[i];
+                    GameMain.data.history.RemoveTechInQueue(i);
+                }
+                GameMain.data.history.EnqueueTech(techId);
+                for (int i = newIndex; i < newQueue.Length; i++)
+                {
+                    GameMain.data.history.EnqueueTech(newQueue[i]);
+                }
+            }
+        }
+    }
+}
