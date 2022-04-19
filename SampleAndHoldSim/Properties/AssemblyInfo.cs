44ef733932de7b92473f75e5fe5611d6dd33484f
starfish <ac355deny@gmail.com>
2022-04-19 13:43:18 +0800

    change name

--- SampleAndHoldSim/Properties/AssemblyInfo.cs
+++ SampleAndHoldSim/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 組件的一般資訊是由下列的屬性集控制。
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
-[assembly: AssemblyTitle("MimicSimulation")]
+[assembly: AssemblyTitle("SampleAndHoldSim")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MimicSimulation")]
+[assembly: AssemblyProduct("SampleAndHoldSim")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
