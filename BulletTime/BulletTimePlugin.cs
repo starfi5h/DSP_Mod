053efe04d703c8f4ec046714f0a3f413db59c9a8
starfish <ac355deny@gmail.com>
2022-09-28 09:31:53 +0800

    BulletTime-v1.2.9
    - Add `RemoveGC`config option.
    - Backward compatible with 0.9.26.13034.

--- BulletTime/BulletTimePlugin.cs
+++ BulletTime/BulletTimePlugin.cs
@@ -15,10 +15,11 @@ namespace BulletTime
     {
         public const string GUID = "com.starfi5h.plugin.BulletTime";
         public const string NAME = "BulletTime";
-        public const string VERSION = "1.2.8";
+        public const string VERSION = "1.2.9";
 
         public static ConfigEntry<bool> EnableBackgroundAutosave;
         public static ConfigEntry<bool> EnableFastLoading;
+        public static ConfigEntry<bool> RemoveGC;
         public static ConfigEntry<KeyboardShortcut> KeyAutosave;
         public static ConfigEntry<float> StartingSpeed;
         public static ConfigEntry<float> MinimumUPS;
@@ -26,11 +27,12 @@ namespace BulletTime
 
         private void LoadConfig()
         {
-            EnableBackgroundAutosave = Config.Bind<bool>("Save", "EnableBackgroundAutosave", true, "Do auto-save in background thread\n在背景执行自动存档");
-            EnableFastLoading = Config.Bind<bool>("Speed", "EnableFastLoading", true, "Increase main menu loading speed\n加快载入主选单");
-            KeyAutosave = Config.Bind("Save", "KeyAutosave", new KeyboardShortcut(KeyCode.F10, KeyCode.LeftShift), "Keyboard shortcut for auto-save\n自动存档的热键组合");
-            StartingSpeed = Config.Bind<float>("Speed", "StartingSpeed", 100f, new ConfigDescription("Game speed when the game begin (0-100)\n游戏开始时的游戏速度 (0-100)", new AcceptableValueRange<float>(0f, 100f)));
             MinimumUPS = Config.Bind<float>("Multiplayer", "MinimumUPS", 50f, new ConfigDescription("Minimum UPS in client of multiplayer game\n联机-客户端的最小逻辑帧"));
+            EnableBackgroundAutosave = Config.Bind<bool>("Save", "EnableBackgroundAutosave", true, "Do auto-save in background thread\n在背景执行自动存档");
+            KeyAutosave = Config.Bind("Save", "KeyAutosave", new KeyboardShortcut(KeyCode.F10, KeyCode.LeftShift), "Keyboard shortcut for auto-save\n自动存档的热键组合");
+            EnableFastLoading = Config.Bind<bool>("Speed", "EnableFastLoading", true, "Increase main menu loading speed\n加快载入主选单");
+            RemoveGC = Config.Bind<bool>("Speed", "RemoveGC", true, "Remove force garbage collection of build tools\n移除建筑工具的强制内存回收");
+            StartingSpeed = Config.Bind<float>("Speed", "StartingSpeed", 100f, new ConfigDescription("Game speed when the game begin (0-100)\n游戏开始时的游戏速度 (0-100)", new AcceptableValueRange<float>(0f, 100f)));
         }
 
         public void Start()
@@ -46,7 +48,28 @@ namespace BulletTime
                 if (EnableBackgroundAutosave.Value)
                     harmony.PatchAll(typeof(GameSave_Patch));
                 if (EnableFastLoading.Value)
-                    harmony.PatchAll(typeof(GameLoader_Patch));
+                {
+                    try
+                    {
+                        harmony.PatchAll(typeof(GameLoader_Patch));
+                    }
+                    catch
+                    {
+                        Log.Warn("Fast loading patch didn't success!");
+                    }
+                }
+                if (RemoveGC.Value)
+                {
+                    try
+                    {
+                        harmony.PatchAll(typeof(BuildTool_Patch));
+                    }
+                    catch
+                    {
+                        Log.Warn("BuildTool no GC patch didn't success!");
+                    }
+                }
+
                 if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey(NebulaCompat.GUID))
                     NebulaCompat.Init(harmony);
             }
