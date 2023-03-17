329d85c0280d19a6e6c9c070985e867f19da50e9
starfish <50672801+starfi5h@users.noreply.github.com>
2023-03-17 18:18:36 +0800

    AlterTickrate-v0.1.0
    - Initial release. (DSP 0.9.27.15466)

---
+++ AlterTickrate/src/Parameters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AlterTickrate
+{
+    public static class Parameters
+    {
+        public static int FacilityUpdatePeriod = 1;
+        public static int InserterUpdatePeriod = 1;
+        public static int StorageUpdatePeriod = 1;
+        public static int BeltUpdatePeriod = 1;
+
+        public static float FacilitySpeedRate = 1.0f;
+        public static float InserterSpeedRate = 1.0f;
+        public static int InserterWaitPeriod = 1;
+
+        public static PlanetFactory AnimOnlyFactory = null;
+        public static CargoContainer LocalCargoContainer = null;
+
+        public static void SetValues(int facilityPeriod, int inserterPeriod, int storageUpdatePeriod, int beltUpdatePeriod)
+        {
+            FacilityUpdatePeriod = facilityPeriod;
+            FacilitySpeedRate = facilityPeriod;
+
+            InserterUpdatePeriod = inserterPeriod;
+            InserterSpeedRate = inserterPeriod;
+            InserterWaitPeriod = (facilityPeriod / inserterPeriod + (facilityPeriod % inserterPeriod > 0 ? 1 : 0)) * inserterPeriod;
+
+            StorageUpdatePeriod = storageUpdatePeriod;
+
+            BeltUpdatePeriod = Math.Min(beltUpdatePeriod, 2); // Max allowance: 2
+        }
+    }
+}
