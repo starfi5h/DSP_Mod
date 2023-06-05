dbba0c2275ca0d0707ca3099fc4314de8581f825
starfish <50672801+starfi5h@users.noreply.github.com>
2023-06-05 19:39:51 +0800

    Fix inseter idleTick

--- AlterTickrate/src/Patches/Inserter_Patch.cs
+++ AlterTickrate/src/Patches/Inserter_Patch.cs
@@ -1,4 +1,8 @@
 ﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace AlterTickrate.Patches
 {
@@ -13,6 +17,39 @@ namespace AlterTickrate.Patches
 			{
 				// scale speed of transferring cargo by x
 				power *= Parameters.InserterSpeedRate;
+			}
+		}
+
+		[HarmonyTranspiler]
+		[HarmonyPatch(typeof(InserterComponent), nameof(InserterComponent.InternalUpdate))]
+		[HarmonyPatch(typeof(InserterComponent), nameof(InserterComponent.InternalUpdateNoAnim))]
+		static IEnumerable<CodeInstruction> InputUpdate_Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			try
+			{
+				var codeMatcher = new CodeMatcher(instructions);
+
+				// Change: this.idleTick = num - 1;
+				// To:     this.idleTick = num - Parameters.InserterUpdatePeriod;
+				codeMatcher
+					.MatchForward(false,
+						new CodeMatch(i => i.IsLdloc()),
+						new CodeMatch(OpCodes.Ldc_I4_1),
+						new CodeMatch(OpCodes.Sub),
+						new CodeMatch(i => i.opcode == OpCodes.Stfld && ((FieldInfo)i.operand).Name == "idleTick")
+					)
+					.Repeat(matcher => matcher
+						.Advance(1)
+						.SetAndAdvance(OpCodes.Ldsfld, AccessTools.Field(typeof(Parameters), nameof(Parameters.InserterUpdatePeriod)))
+					);
+
+				return codeMatcher.InstructionEnumeration();
+			}
+			catch (Exception e)
+			{
+				Log.Error("Transpiler InserterComponent.InternalUpdate failed");
+				Log.Error(e);
+				return instructions;
 			}
 		}
 	}
