d0e485964899b5a59d714ff6f869ece41fe4e820
starfish <50672801+starfi5h@users.noreply.github.com>
2023-06-06 00:23:34 +0800

    inserter fix 2

--- AlterTickrate/src/Patches/Inserter_Patch.cs
+++ AlterTickrate/src/Patches/Inserter_Patch.cs
@@ -9,6 +9,19 @@ namespace AlterTickrate.Patches
 	public class Inserter_Patch
 	{
 		[HarmonyPrefix, HarmonyPriority(Priority.High)]
+		[HarmonyPatch(typeof(FactorySystem), nameof(FactorySystem.GameTickInserters), new Type[] { typeof(long), typeof(bool), typeof(int), typeof(int) })]
+		static void GameTickInserters_Prefix(ref long time)
+		{
+			// bool flag = time % 60L == 0L && isActive;
+			// 讓flag可以在當地星球每60 * Parameters.InserterUpdatePeriod tick觸發
+			time /= Parameters.InserterUpdatePeriod;
+		}
+
+		// 已知: 在buffer的一個貨物結構中(InsertCargoDirect), 10個位置中有6個常數可以幫助定位
+		// 根據鴿籠定理, TryPickItem檢查index~index+5的範圍內必能定位到貨物
+		// 因為藍帶最快5buffer/s因此可以每2tick檢查1次
+
+		[HarmonyPrefix, HarmonyPriority(Priority.High)]
 		[HarmonyPatch(typeof(InserterComponent), nameof(InserterComponent.InternalUpdate))]
 		[HarmonyPatch(typeof(InserterComponent), nameof(InserterComponent.InternalUpdateNoAnim))]
 		static void InternalUpdatePrefix(ref float power)
