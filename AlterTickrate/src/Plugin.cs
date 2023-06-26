fff06836c98b17b9829885949611b2ff4e1dd3c0
starfish <50672801+starfi5h@users.noreply.github.com>
2023-06-26 08:12:00 +0800

    Add UIProgress_Patch for facilities

--- AlterTickrate/src/Plugin.cs
+++ AlterTickrate/src/Plugin.cs
@@ -26,6 +26,7 @@ namespace AlterTickrate
         ConfigEntry<int> Period_SorterUpdate;
         ConfigEntry<int> Period_StorageUpdate;
         ConfigEntry<int> Period_BeltUpdate;
+        ConfigEntry<bool> UI_SmoothProgress;
 
         public void LoadConfig()
         {
@@ -37,6 +38,7 @@ namespace AlterTickrate
             Period_SorterUpdate = Config.Bind("Transport", "Sorter", 2, "Update sorters every x ticks.\n每x帧更新一次分拣器");
             Period_StorageUpdate = Config.Bind("Transport", "Storage", 2, "Update storage every x ticks.\n每x帧更新一次仓储");
             Period_BeltUpdate = Config.Bind("Transport", "Belt", 1, "Update belt every x ticks.(Max:2)\n每x帧更新一次传送带(最大:2)");
+            UI_SmoothProgress = Config.Bind("UI", "SmoothProgress", true, "Interpolates progress animation in UI.\n利用插植使進度動畫平滑");
         }
 
         public void SaveConfig(int beltUpdate, int storageUpdate)
@@ -105,6 +107,10 @@ namespace AlterTickrate
             if (Parameters.BeltUpdatePeriod > 1)
             {
                 harmony.PatchAll(typeof(CargoPath_Patch));
+            }
+            if (UI_SmoothProgress.Value)
+            {
+                harmony.PatchAll(typeof(UIProgress_Patch));
             }
             harmony.PatchAll(typeof(UIcontrol));
         }
