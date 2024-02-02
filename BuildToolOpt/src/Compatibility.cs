1d262dfde30f88bdc1c6b675fa1b0672313aec8c
starfish <50672801+starfi5h@users.noreply.github.com>
2024-02-03 04:48:03 +0800

    [BuildToolOpt] v1.0.3

--- BuildToolOpt/src/Compatibility.cs
+++ BuildToolOpt/src/Compatibility.cs
@@ -9,35 +9,8 @@ namespace BuildToolOpt
     {
         public static void Init(Harmony harmony)
         {
-            DeliverySlotsTweaks_Patch.Init();
             Nebula_Patch.Init();
             CheatEnabler_Patch.Init(harmony);
-        }
-
-        public static class DeliverySlotsTweaks_Patch
-        {
-            public const string GUID = "starfi5h.plugin.DeliverySlotsTweaks";
-
-            public static void Init()
-            {
-                if (!BepInEx.Bootstrap.Chainloader.PluginInfos.TryGetValue(GUID, out var pluginInfo)) return;
-
-                try
-                {
-                    Assembly assembly = pluginInfo.Instance.GetType().Assembly;
-                    Type classType = assembly.GetType("DeliverySlotsTweaks.Plugin");
-
-                    var entry = (ConfigEntry<bool>)(AccessTools.Field(classType, "EnableHologram").GetValue(null));
-                    Plugin.EnableHologram = entry == null || !entry.Value;
-
-                    Plugin.Log.LogDebug("DeliverySlotsTweaks hologram=" + Plugin.EnableHologram);
-                }
-                catch (Exception e)
-                {
-                    Plugin.Log.LogWarning("DeliverySlotsTweaks compatibility failed! Last working version: 1.5.0");
-                    Plugin.Log.LogWarning(e);
-                }
-            }
         }
 
         public static class Nebula_Patch
